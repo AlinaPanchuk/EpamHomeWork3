f9132ed98d7224a0e46f2d66584de7c4faa787ef
AlinaPanchuk <47641712+AlinaPanchuk@users.noreply.github.com>
2019-03-04 10:02:45 +0200

    Add files via upload

--- Task4.java
+++ Task4.java
@@ -3,38 +3,38 @@ package com.Epam.company.HomeWork3.task4;
 public class Task4 {
     public static void main(String[] args) {
         {
-            int i = 0;
+            int lines = 0;
             int numberOfLines = 5;
-            while (i < numberOfLines) {
-                int j = 0;
-                while (j < numberOfLines - i) {
-                    j++;
+            while (lines < numberOfLines) {
+                int rows = 0;
+                while (rows < numberOfLines - lines) {
+                    rows++;
                     System.out.print(" ");
                 }
-                j = i + 1;
-                while (j > 0) {
+                rows = lines + 1;
+                while (rows > 0) {
                     System.out.print("*");
-                    j--;
+                    rows--;
                 }
-                i++;
+                lines++;
                 System.out.println(" ");
             }
         }
         {
-            int i = 0;
+            int lines = 0;
             int numberOfLines = 6;
-            while (i < numberOfLines) {
-                int j = i;
-                while (j > 0) {
-                    j--;
+            while (lines < numberOfLines) {
+                int rows = lines;
+                while (rows > 0) {
+                    rows--;
                     System.out.print(" ");
                 }
-                j = 0 ;
-                while (j < numberOfLines -i) {
+                rows = 0 ;
+                while (rows < numberOfLines -lines) {
                     System.out.print("*");
-                    j++;
+                    rows++;
                 }
-                i++;
+                lines++;
                 System.out.println(" ");
             }
         }
