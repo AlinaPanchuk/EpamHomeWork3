941d4d10dbfeee22eed6c76df8cc9b453cd7df14
AlinaPanchuk <47641712+AlinaPanchuk@users.noreply.github.com>
2019-03-04 10:01:47 +0200

    Add files via upload

--- Task3.java
+++ Task3.java
@@ -3,23 +3,22 @@ package com.Epam.company.HomeWork3.task3;
 public class Task3 {
     public static void main(String[] args) {
         {
-            int i = 0;
+            int lines = 0;
             int numberOfLines = 6;
-            while (i < numberOfLines) {
-                int j = 0;
-                while (j < numberOfLines - i) {
-                    j++;
+            while (lines < numberOfLines) {
+                int rows = 0;
+                while (rows < numberOfLines - lines) {
+                    rows++;
                     System.out.print(" ");
                 }
-                j = i + 1;
-                while (j > 0) {
+                rows = lines + 1;
+                while (rows > 0) {
                     System.out.print("*");
-                    j--;
+                    rows--;
                 }
-                i++;
+                lines++;
                 System.out.println(" ");
             }
         }
-
     }
 }
